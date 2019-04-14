3c4c610db4d7fea4a0d0cfc5658612bf055820e6
Tien Pham <r3dsky02@yahoo.com.vn>
2019-04-14 20:00:19 +0300

    Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+import Post from './components/Post';
+
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/post/:postId" component={Post} />
+          </Switch>
+        </Router>
       </div>
     );
   }
